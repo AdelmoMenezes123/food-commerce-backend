af2653fa2c53966b8e173d858141c0a469bfde37
Adelmo Menezes <adelmo@connectlab.com.br>
2025-01-19 15:36:16 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+export default app;
